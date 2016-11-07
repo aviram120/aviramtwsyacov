ee3faee55c36ddb8de43ed290d0cb52ba5dd1e97
yaakov300 <yaacov300@gmail.com>
2016-11-07 15:15:47 +0200

    open and save files to GUI

--- src/api2/JframeVaribalseLocGlob.java
+++ src/api2/JframeVaribalseLocGlob.java
@@ -1,6 +1,17 @@
 package api2;
 
 
+import java.awt.EventQueue; 
+import javax.imageio.ImageIO;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.File;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.awt.Color;
+
+
 import ayAPI.globalVar;
 import ayAPI.localVar;
 
@@ -39,6 +50,7 @@ import javax.swing.JFormattedTextField;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import javax.swing.SwingConstants;
 
 public class JframeVaribalseLocGlob extends JFrame {
 	private HTTP httpRequest = new HTTP();
@@ -50,7 +62,7 @@ public class JframeVaribalseLocGlob extends JFrame {
 	//gui element
 	JPanel contentPane;
 	JPanel panel;
-	JLabel lblLoacal;
+	JLabel lblGlobal;
 	JLabel lblNewLabel;
 	JLabel lblOrdertype;
 	JLabel lblCenttogiveup;
@@ -94,7 +106,9 @@ public class JframeVaribalseLocGlob extends JFrame {
 	JButton btnNewButton;
 	JButton btnStop;
 	JLabel lblNewLabel_1;
-	JComboBox comboBoxOrderType;	
+	JComboBox comboBoxOrderType;
+	JButton btnOpenFile;
+	JButton btnSaveFile;
 
 	String[] stringsOrderType = { "Market", "Stop limit" };//1,2
 	Map<String, Integer> mapOrderType = new HashMap<String, Integer>();
@@ -130,7 +144,7 @@ public class JframeVaribalseLocGlob extends JFrame {
 
 	public JframeVaribalseLocGlob() {
 		//if(authentication())
-			initialize();
+		initialize();
 	}
 
 	public void initialize(){	
@@ -180,164 +194,165 @@ public class JframeVaribalseLocGlob extends JFrame {
 		panel = new JPanel();		
 		contentPane.add(panel, BorderLayout.CENTER);
 
-		lblLoacal = new JLabel("global");
-		lblLoacal.setBounds(37, 11, 38, 16);
-		lblLoacal.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblGlobal = new JLabel("global");
+		lblGlobal.setBounds(10, 47, 38, 16);
+		lblGlobal.setFont(new Font("Tahoma", Font.BOLD, 13));
 
 		lblNewLabel = new JLabel("local");
-		lblNewLabel.setBounds(303, 11, 29, 16);
+		lblNewLabel.setBounds(273, 45, 38, 16);
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 13));
 
 		lblOrdertype = new JLabel("orderType");
-		lblOrdertype.setBounds(10, 42, 145, 16);
+		lblOrdertype.setBounds(10, 74, 145, 16);
 		lblOrdertype.setFont(new Font("Tahoma", Font.PLAIN, 13));
 
 		lblCenttogiveup = new JLabel("centToGiveup");
-		lblCenttogiveup.setBounds(10, 68, 85, 16);
+		lblCenttogiveup.setBounds(10, 100, 85, 16);
 		lblCenttogiveup.setFont(new Font("Tahoma", Font.PLAIN, 13));
 
 		lblDefinenextstop = new JLabel("defineNextStop");
-		lblDefinenextstop.setBounds(10, 94, 91, 16);
+		lblDefinenextstop.setBounds(10, 126, 91, 16);
 		lblDefinenextstop.setFont(new Font("Tahoma", Font.PLAIN, 13));
 
 		lblStoptype = new JLabel("stopType");
-		lblStoptype.setBounds(10, 120, 91, 16);
+		lblStoptype.setBounds(10, 152, 91, 16);
 		lblStoptype.setFont(new Font("Tahoma", Font.PLAIN, 13));
 
 		lblMaxrisk = new JLabel("maxRisk");
-		lblMaxrisk.setBounds(10, 146, 91, 16);
+		lblMaxrisk.setBounds(10, 178, 91, 16);
 		lblMaxrisk.setFont(new Font("Tahoma", Font.PLAIN, 13));
 
 		lblTimestopaddorder = new JLabel("timeStopAddOrder");
-		lblTimestopaddorder.setBounds(10, 172, 118, 16);
+		lblTimestopaddorder.setBounds(10, 204, 118, 16);
 		lblTimestopaddorder.setFont(new Font("Tahoma", Font.PLAIN, 13));
 
 		lblTimecloseallorder = new JLabel("timeCloseAllOrder");
-		lblTimecloseallorder.setBounds(10, 198, 118, 16);
+		lblTimecloseallorder.setBounds(10, 230, 118, 16);
 		lblTimecloseallorder.setFont(new Font("Tahoma", Font.PLAIN, 13));
 
 		lblSymbol = new JLabel("symbol");
-		lblSymbol.setBounds(273, 42, 118, 16);
+		lblSymbol.setBounds(273, 73, 118, 16);
 		lblSymbol.setFont(new Font("Tahoma", Font.PLAIN, 13));
 
 		textFieldSymbol = new JTextField();
-		textFieldSymbol.setBounds(471, 40, 86, 20);
+		textFieldSymbol.setBounds(471, 71, 86, 20);
 		textFieldSymbol.setColumns(10);
 
 		lblStrategy = new JLabel("strategy");
-		lblStrategy.setBounds(273, 68, 118, 16);
+		lblStrategy.setBounds(273, 99, 118, 16);
 		lblStrategy.setFont(new Font("Tahoma", Font.PLAIN, 13));
 
 		lblDirection = new JLabel("direction");
-		lblDirection.setBounds(273, 94, 118, 16);
+		lblDirection.setBounds(273, 125, 118, 16);
 		lblDirection.setFont(new Font("Tahoma", Font.PLAIN, 13));
 
 		lblMovingAvr = new JLabel("movingAvr");
-		lblMovingAvr.setBounds(273, 120, 118, 16);
+		lblMovingAvr.setBounds(273, 151, 118, 16);
 		lblMovingAvr.setFont(new Font("Tahoma", Font.PLAIN, 13));			
 
 		lblInterval = new JLabel("interval");
-		lblInterval.setBounds(273, 146, 118, 16);
+		lblInterval.setBounds(273, 177, 118, 16);
 		lblInterval.setFont(new Font("Tahoma", Font.PLAIN, 13));
 
 		lblMinvolume = new JLabel("minVolume");
-		lblMinvolume.setBounds(273, 172, 118, 16);
+		lblMinvolume.setBounds(273, 203, 118, 16);
 		lblMinvolume.setFont(new Font("Tahoma", Font.PLAIN, 13));
 
 		lblMinbarsize = new JLabel("minBarSize");
-		lblMinbarsize.setBounds(273, 198, 118, 16);
+		lblMinbarsize.setBounds(273, 229, 118, 16);
 		lblMinbarsize.setFont(new Font("Tahoma", Font.PLAIN, 13));
 
 		lblMaxbarsize = new JLabel("maxBarSize");
-		lblMaxbarsize.setBounds(273, 224, 118, 16);
+		lblMaxbarsize.setBounds(273, 255, 118, 16);
 		lblMaxbarsize.setFont(new Font("Tahoma", Font.PLAIN, 13));
 
 		lblIsaggressive = new JLabel("isAggressive");
-		lblIsaggressive.setBounds(273, 302, 118, 16);
+		lblIsaggressive.setBounds(273, 333, 118, 16);
 		lblIsaggressive.setFont(new Font("Tahoma", Font.PLAIN, 13));
 
 		lblAddcenttobreak = new JLabel("addCentToBreak");
-		lblAddcenttobreak.setBounds(273, 250, 118, 16);
+		lblAddcenttobreak.setBounds(273, 281, 118, 16);
 		lblAddcenttobreak.setFont(new Font("Tahoma", Font.PLAIN, 13));
 
 		lblNumbartocanceldeal = new JLabel("numBarToCancelDeal");
-		lblNumbartocanceldeal.setBounds(273, 276, 136, 16);
+		lblNumbartocanceldeal.setBounds(273, 307, 136, 16);
 		lblNumbartocanceldeal.setFont(new Font("Tahoma", Font.PLAIN, 13));
 
 		lblMaxtransactionsperday = new JLabel("maxTransactionsPerDay");
-		lblMaxtransactionsperday.setBounds(273, 328, 153, 16);
+		lblMaxtransactionsperday.setBounds(273, 359, 153, 16);
 		lblMaxtransactionsperday.setFont(new Font("Tahoma", Font.PLAIN, 13));
 
 		lblRiskpertransactionsdolars = new JLabel("riskPerTransactionsDolars");
-		lblRiskpertransactionsdolars.setBounds(273, 354, 153, 16);
+		lblRiskpertransactionsdolars.setBounds(273, 385, 153, 16);
 		lblRiskpertransactionsdolars.setFont(new Font("Tahoma", Font.PLAIN, 13));
 
 		lblMaxriskpertransactionsdolars = new JLabel("maxRiskPerTransactionsDolars");
-		lblMaxriskpertransactionsdolars.setBounds(273, 380, 188, 16);
+		lblMaxriskpertransactionsdolars.setBounds(273, 411, 188, 16);
 		lblMaxriskpertransactionsdolars.setFont(new Font("Tahoma", Font.PLAIN, 13));
 
 		lblExtarprice = new JLabel("extarPrice");
-		lblExtarprice.setBounds(273, 406, 118, 16);
+		lblExtarprice.setBounds(273, 437, 118, 16);
 		lblExtarprice.setFont(new Font("Tahoma", Font.PLAIN, 13));
 
 		lblPe = new JLabel("pe");
-		lblPe.setBounds(273, 432, 118, 16);
+		lblPe.setBounds(273, 463, 118, 16);
 		lblPe.setFont(new Font("Tahoma", Font.PLAIN, 13));
 
 		lblBartriger = new JLabel("barTriger");
-		lblBartriger.setBounds(273, 458, 118, 16);
+		lblBartriger.setBounds(273, 489, 118, 16);
 		lblBartriger.setFont(new Font("Tahoma", Font.PLAIN, 13));
 
 		spinnerCentToGiveup = new JSpinner();
 		spinnerCentToGiveup.setModel(new SpinnerNumberModel(new Double(0), null, null, new Double(1)));
-		spinnerCentToGiveup.setBounds(138, 66, 86, 20);
+		spinnerCentToGiveup.setBounds(138, 98, 86, 20);
 
 		spinnerDefineNextStop = new JSpinner();
-		spinnerDefineNextStop.setBounds(138, 92, 86, 20);
+		spinnerDefineNextStop.setBounds(138, 124, 86, 20);
 
 		spinnerMaxRisk = new JSpinner();
-		spinnerMaxRisk.setBounds(138, 144, 86, 20);
+		spinnerMaxRisk.setModel(new SpinnerNumberModel(new Double(0), null, null, new Double(1)));
+		spinnerMaxRisk.setBounds(138, 176, 86, 20);
 
 		spinnerMinVolume = new JSpinner();
-		spinnerMinVolume.setBounds(471, 170, 86, 20);
+		spinnerMinVolume.setBounds(471, 201, 86, 20);
 
 		spinnerMinBarSize = new JSpinner();
 		spinnerMinBarSize.setModel(new SpinnerNumberModel(new Double(0), null, null, new Double(1)));
-		spinnerMinBarSize.setBounds(471, 196, 86, 20);
+		spinnerMinBarSize.setBounds(471, 227, 86, 20);
 
 		spinnerMaxBarSize = new JSpinner();
 		spinnerMaxBarSize.setModel(new SpinnerNumberModel(new Double(0), null, null, new Double(1)));
-		spinnerMaxBarSize.setBounds(471, 222, 86, 20);
+		spinnerMaxBarSize.setBounds(471, 253, 86, 20);
 
 		spinnerAddCentToBreak = new JSpinner();
 		spinnerAddCentToBreak.setModel(new SpinnerNumberModel(new Double(0), null, null, new Double(1)));
-		spinnerAddCentToBreak.setBounds(471, 248, 86, 20);
+		spinnerAddCentToBreak.setBounds(471, 279, 86, 20);
 
 		spinnerNumBarToCancelDeal = new JSpinner();
-		spinnerNumBarToCancelDeal.setBounds(471, 274, 86, 20);
+		spinnerNumBarToCancelDeal.setBounds(471, 305, 86, 20);
 
 		spinnerMaxTransactionsPerDay = new JSpinner();
-		spinnerMaxTransactionsPerDay.setBounds(471, 326, 86, 20);
+		spinnerMaxTransactionsPerDay.setBounds(471, 357, 86, 20);
 
 		spinnerRiskPerTransactionsDolars = new JSpinner();
 		spinnerRiskPerTransactionsDolars.setModel(new SpinnerNumberModel(new Double(0), null, null, new Double(1)));
-		spinnerRiskPerTransactionsDolars.setBounds(471, 352, 86, 20);
+		spinnerRiskPerTransactionsDolars.setBounds(471, 383, 86, 20);
 
 		spinnerMaxRiskPerTransactionsDolars = new JSpinner();
 		spinnerMaxRiskPerTransactionsDolars.setModel(new SpinnerNumberModel(new Double(0), null, null, new Double(1)));
-		spinnerMaxRiskPerTransactionsDolars.setBounds(471, 379, 86, 20);
+		spinnerMaxRiskPerTransactionsDolars.setBounds(471, 410, 86, 20);
 
 		spinnerExtarPrice = new JSpinner();
 		spinnerExtarPrice.setModel(new SpinnerNumberModel(new Double(0), null, null, new Double(1)));
-		spinnerExtarPrice.setBounds(471, 404, 86, 20);
+		spinnerExtarPrice.setBounds(471, 435, 86, 20);
 
 		spinnerPe = new JSpinner();
 		spinnerPe.setModel(new SpinnerNumberModel(new Double(0), null, null, new Double(1)));
-		spinnerPe.setBounds(471, 430, 86, 20);
+		spinnerPe.setBounds(471, 461, 86, 20);
 
 		spinnerBarTriger = new JSpinner();
 		spinnerBarTriger.setModel(new SpinnerNumberModel(new Double(0), null, null, new Double(1)));
-		spinnerBarTriger.setBounds(471, 456, 86, 20);
+		spinnerBarTriger.setBounds(471, 487, 86, 20);
 
 		btnNewButton = new JButton("start");
 		btnNewButton.addActionListener(new ActionListener() {
@@ -357,42 +372,109 @@ public class JframeVaribalseLocGlob extends JFrame {
 		lblNewLabel_1.setBounds(10, 553, 65, 14);
 
 		comboBoxOrderType = new JComboBox(stringsOrderType);		
-		comboBoxOrderType.setBounds(138, 41, 86, 20);
+		comboBoxOrderType.setBounds(138, 73, 86, 20);
 
 		comboBoxStopType = new JComboBox(stringsStopType);
-		comboBoxStopType.setBounds(138, 118, 86, 20);
+		comboBoxStopType.setBounds(138, 150, 86, 20);
 
 		spinnerTimeStopaddOrder = new JSpinner(spinMod1);
 		spinnerTimeStopaddOrder.setEditor(new JSpinner.DateEditor(spinnerTimeStopaddOrder,"HH:mm"));
-		spinnerTimeStopaddOrder.setBounds(138, 170, 86, 20);
+		spinnerTimeStopaddOrder.setBounds(138, 202, 86, 20);
 
 		spinnerTimeCloseAllOrder = new JSpinner(spinMod2);
 		spinnerTimeCloseAllOrder.setEditor(new JSpinner.DateEditor(spinnerTimeCloseAllOrder,"HH:mm"));
-		spinnerTimeCloseAllOrder.setBounds(138, 196, 86, 20);
+		spinnerTimeCloseAllOrder.setBounds(138, 228, 86, 20);
 
 		comboBoxStrategy = new JComboBox(stringsStrategy);
-		comboBoxStrategy.setBounds(471, 66, 86, 20);
+		comboBoxStrategy.setBounds(471, 97, 86, 20);
 
 		comboBoxDirection = new JComboBox(stringsDirection);
-		comboBoxDirection.setBounds(471, 92, 86, 20);
+		comboBoxDirection.setBounds(471, 123, 86, 20);
 
 		comboBoxMovingAvr = new JComboBox(stringsMovingAvr);
-		comboBoxMovingAvr.setBounds(471, 118, 85, 20);
+		comboBoxMovingAvr.setBounds(471, 149, 85, 20);
 
 		comboBoxInterval = new JComboBox(stringsInterval);
-		comboBoxInterval.setBounds(471, 144, 86, 20);
+		comboBoxInterval.setBounds(471, 175, 86, 20);
 
 		comboBoxIsAggressive = new JComboBox(stringsIsAggressive);
-		comboBoxIsAggressive.setBounds(471, 300, 86, 20);
+		comboBoxIsAggressive.setBounds(471, 331, 86, 20);
 
 		spinnerThreadId = new JSpinner(modelTheradNumber);		
 		spinnerThreadId.setBounds(71, 550, 86, 20);
 
+		btnOpenFile = new JButton("Open");
+		btnOpenFile.setBounds(10, 11, 89, 23);				
+		btnOpenFile.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) 
+			{								
+				JFileChooser fc = new JFileChooser();//create a file open dialog
+				fc.setCurrentDirectory(new File(System.getProperty("user.dir")));//+"\\favorite"));//the folder path to Images directory that is open
+				//FileFilter imageFilter = new FileNameExtensionFilter("Image files", ImageIO.getReaderFileSuffixes());//filter for image only "jpg jpeg bmp png wbmp gif"
+				//fc.addChoosableFileFilter(imageFilter);
+				fc.setAcceptAllFileFilterUsed(true);
+				if(fc.showOpenDialog(null)==JFileChooser.APPROVE_OPTION)
+				{
+					readFileToGUI(fc.getSelectedFile().getAbsolutePath());
+					//lblNewLabel.setText(fc.getSelectedFile().getAbsolutePath());
+				}
+			}
+		});
+		
+		btnSaveFile = new JButton("Save");
+		btnSaveFile.setBounds(112, 11, 89, 23);
+		btnSaveFile.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) 
+			{								
+				JFileChooser fc = new JFileChooser();//create a file open dialog
+				fc.setCurrentDirectory(new File(System.getProperty("user.dir")));//+"\\favorite"));//the folder path to Images directory that is open				
+				fc.setAcceptAllFileFilterUsed(true);
+				if(fc.showSaveDialog(null) == JFileChooser.APPROVE_OPTION)
+				{
+					//get globalVar
+					int orderType = mapOrderType.get(stringsOrderType[comboBoxOrderType.getSelectedIndex()]);
+					double centToGiveup = (double) spinnerCentToGiveup.getValue();
+					int defineNextStop = (int) spinnerDefineNextStop.getValue();
+					int stopType = mapStopType.get(stringsStopType[comboBoxStopType.getSelectedIndex()]);
+					double maxRisk = (double) spinnerMaxRisk.getValue();		
+					Calendar calendar = GregorianCalendar.getInstance();
+					calendar.setTime((Date) spinnerTimeStopaddOrder.getValue()); 								
+					String timeStopAddOrder = "" + calendar.get(Calendar.HOUR_OF_DAY) + ":" + calendar.get(Calendar.MINUTE);
+					calendar.setTime((Date) spinnerTimeCloseAllOrder.getValue());		
+					String timeCloseAllOrder = "" + calendar.get(Calendar.HOUR_OF_DAY) + ":" + calendar.get(Calendar.MINUTE);
+					globalVar tempGlobal = new globalVar(orderType,centToGiveup,defineNextStop,stopType,maxRisk,timeStopAddOrder, timeCloseAllOrder);
+
+					//get localVar
+					String symbol = textFieldSymbol.getText();
+					int strategy = mapStrategy.get(stringsStrategy[comboBoxStrategy.getSelectedIndex()]);
+					int direction = mapDirection.get(stringsDirection[comboBoxDirection.getSelectedIndex()]);		
+					int movingAvr = mapMovingAvr.get(stringsMovingAvr[comboBoxMovingAvr.getSelectedIndex()]);
+					int interval = mapInterval.get(stringsInterval[comboBoxInterval.getSelectedIndex()]);
+					int minVolume = (int) spinnerMinVolume.getValue();
+					double minBarSize = (double) spinnerMinBarSize.getValue();
+					double maxBarSize = (double) spinnerMaxBarSize.getValue();
+					double addCentToBreak = (double) spinnerAddCentToBreak.getValue();
+					int numBarToCancelDeal = (int) spinnerNumBarToCancelDeal.getValue();
+					boolean isAggressive = mapIsAggressive.get(stringsIsAggressive[comboBoxIsAggressive.getSelectedIndex()]);
+					int maxTransactionsPerDay = (int) spinnerMaxTransactionsPerDay.getValue();
+					double riskPerTransactionsDolars = (double) spinnerRiskPerTransactionsDolars.getValue();
+					double maxRiskPerTransactionsDolars = (double) spinnerMaxRiskPerTransactionsDolars.getValue();
+					double extarPrice = (double) spinnerExtarPrice.getValue();
+					double pe = (double) spinnerPe.getValue();
+					double barTriger = (double) spinnerBarTriger.getValue();
+					localVar tempLocal = new localVar(symbol, strategy, direction, movingAvr,interval, minVolume, minBarSize,maxBarSize, addCentToBreak, numBarToCancelDeal, 
+							isAggressive, maxTransactionsPerDay,riskPerTransactionsDolars, maxRiskPerTransactionsDolars, extarPrice, pe, barTriger );
+
+					saveGuiToFile(tempGlobal,tempLocal,fc.getSelectedFile().getAbsolutePath());					
+				}
+			}
+		});
+		
 		panel.setLayout(null);
 		panel.add(lblTimestopaddorder);
 		panel.add(lblTimecloseallorder);
 		panel.add(lblCenttogiveup);
-		panel.add(lblLoacal);
+		panel.add(lblGlobal);
 		panel.add(lblOrdertype);
 		panel.add(lblDefinenextstop);
 		panel.add(lblStoptype);
@@ -400,7 +482,6 @@ public class JframeVaribalseLocGlob extends JFrame {
 		panel.add(spinnerMaxRisk);
 		panel.add(spinnerDefineNextStop);
 		panel.add(spinnerCentToGiveup);
-		panel.add(lblNewLabel);
 		panel.add(lblMaxriskpertransactionsdolars);
 		panel.add(lblExtarprice);
 		panel.add(lblPe);
@@ -433,6 +514,7 @@ public class JframeVaribalseLocGlob extends JFrame {
 		panel.add(btnNewButton);			
 		panel.add(btnStop);			
 		panel.add(lblNewLabel_1);
+		panel.add(lblNewLabel);
 		panel.add(comboBoxOrderType);			
 		panel.add(comboBoxStopType);		
 		panel.add(spinnerTimeStopaddOrder);			
@@ -443,12 +525,13 @@ public class JframeVaribalseLocGlob extends JFrame {
 		panel.add(comboBoxInterval);		
 		panel.add(comboBoxIsAggressive);
 		panel.add(spinnerThreadId);
-
-
+		panel.add(btnOpenFile);
+		panel.add(btnSaveFile);
 	}
+
 	private void readFileToGUI(String fileName)
 	{//the function get a file path(file that include 'global' and 'local' vars) and set the var into GUI 
-		
+
 		String partGlobal = "";
 		String partLocal = "";
 		try {
@@ -460,8 +543,6 @@ public class JframeVaribalseLocGlob extends JFrame {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
-
 
 		globalVar tempGlobal = new globalVar(partGlobal);//from JSON
 
@@ -484,7 +565,6 @@ public class JframeVaribalseLocGlob extends JFrame {
 			e.printStackTrace();
 		}
 
-		
 		localVar tempLocal = new localVar(partLocal);//from JSON
 		comboBoxStrategy.setSelectedItem(getKeyFromValue(mapStrategy,tempLocal.getStrategy()));
 		comboBoxDirection.setSelectedItem(getKeyFromValue(mapDirection,tempLocal.getDirection()));
@@ -506,7 +586,7 @@ public class JframeVaribalseLocGlob extends JFrame {
 
 	private static Object getKeyFromValue(Map hm, Object value) 
 	{//the function searce in map a value
-		
+
 		for (Object o : hm.keySet()) {
 			if (hm.get(o).equals(value)) {
 				return o;
@@ -565,7 +645,7 @@ public class JframeVaribalseLocGlob extends JFrame {
 
 	private void saveGuiToFile(globalVar global, localVar local,String fileName)
 	{// the function seve the GUI in to file
-		
+
 		String stGlobal = global.convertObjToJSON();
 		String stLocal = local.convertObjToJSON();
 
@@ -576,7 +656,7 @@ public class JframeVaribalseLocGlob extends JFrame {
 		BufferedWriter writer = null;
 		try
 		{
-			
+
 			writer = new BufferedWriter( new FileWriter(fileName));
 			writer.write( st);
 
@@ -625,7 +705,7 @@ public class JframeVaribalseLocGlob extends JFrame {
 
 	private static String readFile(String fileName) throws IOException
 	{//the fucntion return string from file
-		
+
 		BufferedReader br = new BufferedReader(new FileReader(fileName));
 		try {
 			StringBuilder sb = new StringBuilder();
